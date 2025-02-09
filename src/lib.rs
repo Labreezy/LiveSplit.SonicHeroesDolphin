23a42a879e4fd859fa4cd781df8ace6d1357b1a0
erobards <emr972@utulsa.edu>
2025-02-09 11:49:59 -0600

    Added support for NTSC-J heroes

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ use asr::{
 };
 use asr::time_util;
 use asr::settings::Gui;
-use bitflags::bitflags;
+
 
 asr::panic_handler!();
 asr::async_main!(stable);
@@ -34,20 +34,25 @@ pub struct Settings {
     ///Automatically start the autosplitter.
     automatically_start: bool,
 }
-
+//G9SE8P
+const GAME_ID_US: [u8;6] = [0x47, 0x39, 0x53, 0x45, 0x38, 0x50];
+//G9SJ8P
+const GAME_ID_JP: [u8;6] = [0x47, 0x39, 0x53, 0x4A, 0x38, 0x50];
 async fn main() {
     let mut settings = Settings::register();
     loop {
         // Hook to the target process
         let mut emulator = retry(|| gcn::Emulator::attach()).await;
         let mut watchers = Watchers::default();
-        let offsets = Offsets::new();
+        let mut offsets = Offsets::default();
         let mut igt_info = IGTInfo::default();
-
+        
 
         loop {
             if !emulator.is_open() {
-
+                if offsets.emblem_screen != 0{
+                    offsets = Offsets::default();
+                }
                 break;
             }
             if emulator.update() {
@@ -58,6 +63,21 @@ async fn main() {
                 // 3. If reset does not return true, then the split action will be run.
                 // 4. If the timer is currently not running (and not paused), then the start action will be run.
                 settings.update();
+                if offsets.emblem_screen == 0{
+                    let game_id = emulator.read::<[u8; 6]>(0x80000000).unwrap_or_default();
+                    for b in game_id {
+                        print_limited::<64>(&format_args!("{b:#02X}"));
+                    }
+                    print_message("-------");
+                    if game_id[3] == 0x4A {
+                        print_message("JP HEROES");
+                    } else if game_id[3] == 0x45 {
+                        print_message("US HEROES");
+                    }
+                    
+                    offsets = Offsets::from_gameid(game_id);
+                    
+                }
                 update_loop(&emulator, &offsets, &mut watchers);
                 let timer_state = timer::state();
                 if timer_state == TimerState::Running {
@@ -82,6 +102,8 @@ async fn main() {
                     }
                     
                 }
+            } else if offsets.emblem_screen != 0{
+                offsets = Offsets::default();
             }
             next_tick().await;
         }
@@ -98,25 +120,47 @@ struct Watchers {
     frame_counter: Watcher<u32>,
     emblem_screen: Watcher<u8>
 }
+
+
+#[derive(Default)]
 struct Offsets {
     frame_counter: u32,
     emblem_screen: u32
 
 }
 
+
 impl Offsets {
-    fn new() -> Self {
-        Self {
-            emblem_screen: 0x42C28F,
-            frame_counter: 0x452C4C,
-            
+    fn from_gameid(game_id: [u8;6]) -> Offsets {
+        
+        let offsets = match game_id {
+            GAME_ID_US => Self {
+                emblem_screen: 0x42C28F,
+                frame_counter: 0x452C4C,
+                
+            }
+        ,
+            GAME_ID_JP => Self {
+                emblem_screen: 0x42C36F,
+                frame_counter: 0x442D4C,
+                
+            },
+            _ => Self::default()
+        };
+        return offsets;
         }
+       
     }
-}
+
+
+
 
 
 fn update_loop(game: &Emulator, offsets: &Offsets, watchers: &mut Watchers) {
-
+   
+    if offsets.emblem_screen == 0 {
+        return
+    }
     let fc = game.read::<u32>(offsets.frame_counter).unwrap_or_default();
     let emblem = game.read::<u8>(offsets.emblem_screen).unwrap_or_default();    
     
@@ -129,7 +173,7 @@ fn update_loop(game: &Emulator, offsets: &Offsets, watchers: &mut Watchers) {
 
 fn start(watchers: &Watchers, settings: &Settings) -> bool {
     if !settings.automatically_start {return false;}
-    if watchers.frame_counter.pair.expect("WHOOPS (START)").changed_from(&0) {
+    if watchers.frame_counter.pair.unwrap_or_default().changed_from(&0) {
         return true;
     }
     return false;
